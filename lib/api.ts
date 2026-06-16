a68f75104bd48469328978195587cdb99bade3bc
Nelson Kouame <kouamenelson47@gmail.com>
2026-06-16 21:00:53 +0300

    feat: add code

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://immob-api-charist-production.up.railway.app/v1";
+const BASE_URL = "https://immob-api-charist-production.up.railway.app/api/v1";
 
 export const api = axios.create({
   baseURL: BASE_URL,
