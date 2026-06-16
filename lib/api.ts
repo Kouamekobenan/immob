562ccaca59c892793c54b0d0984ae994fdfe33dd
Nelson Kouame <kouamenelson47@gmail.com>
2026-06-16 16:49:37 +0300

    add   lib/api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001/api/v1';
+const BASE_URL = "https://immob-api-charist-production.up.railway.app/v1";
 
 export const api = axios.create({
   baseURL: BASE_URL,
